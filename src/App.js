cf7412cde181b1921821cc5c83266670f497e99a
James <jamesnjorio@gmail.com>
2022-10-05 15:29:49 +0300

    Centered every component view

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function App() {
   },[])
 
   return (
-    <div className="container mx-auto bg-teal-500 px-10 py-4 mt-20">
+    <div className="container mx-auto bg-teal-500 px-10  mt-20">
 
       <NavBar onChangePage={setPage}/>
 
        <h1 className='text-center text-white text-xl font-serif py-3'> JAY 5-Star Hotel Orders</h1>
-       <div  className="container mx-auto bg-zinc-500 md:bg-red-500 lg:bg-emarald-500 px-4">
+       <div  className="max-w-md mx-auto rounded-xl bg-zinc-500 md:bg-red-500 lg:bg-emarald-500 px-4">
           <Routes>
 
             <Route path='/' element={<PlaceOrder/>}/>
@@ -35,7 +35,7 @@ function App() {
 
           </Routes>
     </div>
-    <p> Working Together with You | copyright@2022</p>
+    <p className='text-center py-3 italic'> Working Together with You | copyright@2022</p>
     </div>
    
   );
